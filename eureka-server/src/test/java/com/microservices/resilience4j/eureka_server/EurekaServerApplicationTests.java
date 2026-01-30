e28ed72f55853cfb1f9de656e07861f82c4777e8
Luis Balarezo <luis.balarezo@unmsm.edu.pe>
2026-01-30 11:17:40 -0500

    feat: init commit

---
+++ eureka-server/src/test/java/com/microservices/resilience4j/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.resilience4j.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
